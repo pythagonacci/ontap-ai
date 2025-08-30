51a99c584bbdc01db887897b10010c44dae7a79d
aahmadamna <aahmadamna@gmail.com>
2025-08-30 15:41:58 -0400

    icons

--- frontend/src/extension/manifest.ts
+++ frontend/src/extension/manifest.ts
@@ -18,10 +18,10 @@ export default defineManifest({
   action: { 
     default_title: "Ontap AI",
     default_icon: {
-      "16": "icons/icon-16.png",
-      "32": "icons/icon-32.png",
-      "48": "icons/icon-48.png",
-      "128": "icons/icon-128.png"
+      "16": "icons/icon16.png",
+      "32": "icons/icon32.png",
+      "48": "icons/icon48.png",
+      "128": "icons/icon128.png"
     }
   },
   commands: {
